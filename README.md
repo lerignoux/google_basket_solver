de0feec394bdcb3787fb093d4e916084766e733f
Laurent Erignoux <lerignoux@gmail.com>
2016-08-19 02:30:50 +0200

    Adding simple script solving the basket minigame

---
+++ README.md
@@ -0,0 +1,8 @@
+# Google basket solver.
+
+A simple script to try to solve the mini google basket ball game
+
+Script mainly use xdotool to simulate user clicks.
+you can install it on most ubuntu systems with your favorite package manager.
+
+Project did not succeed in wining the game. The probable cause is the imprecision in xdotools click times.
---
+++ Try.sh
@@ -0,0 +1,21 @@
+firefox https://www.google.com/doodles/basketball-2012
+sleep 2
+
+xdotool mousemove --sync 2300 400
+xdotool click 1
+
+sleep 0.7
+xdotool key space
+sleep 0.2
+xdotool key --delay 400 space space space space space space space space space
+sleep 0.5
+
+
+#Deuxieme serie
+#xdotool key --delay 800 space  --delay 50 space  --delay 800 space  --delay 50 space  --delay 800 space  --delay 50 space --delay 800 space  --delay 50 space 
+
+#Troisieme serie
+#xdotool key --delay 900 space  --delay 50 space  --delay 900 space  --delay 50 space --delay 900 space  --delay 50 space
+
+
+#xdotool key --delay 1000 space  --delay 50 space  --delay 1000 space 
